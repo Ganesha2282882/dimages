09783f1c925838c801822c5e4231b77e9d39598c
Ganesha Sharma <59745725+Ganesha2282882@users.noreply.github.com>
2020-08-13 13:40:19 -0400

    Added some religion stuff

---
+++ ganeshdocker/main.sh
@@ -0,0 +1,4 @@
+mantra="Om Gam Ganpataye Namah"
+love="Ganesh, I love you!"
+echo $mantra
+echo $love
---
+++ hgoddocker/main.py
@@ -0,0 +1,5 @@
+from random import choice
+gods = (["Brahma", "Vishnu", "Shiva", "Ganesha", "Rama", "Krishna", "Saraswati", "Lakshmi", "Durga Devi", "Indra", "Surya", "Agni", "Hanuman"])
+random_god = choice(gods)
+print(random_god)
+
